6742c83573f6b4183744242b58f646f582f5c436
meou19 <dongphuong.meou19@gmail.com>
2021-07-08 22:34:58 +0700

    alo

---
+++ src/app/customerwebsite/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { data } from 'jquery';
+import jwt_decode from 'jwt-decode';
+import { CustomerAcc } from 'src/app/model/CustomerAcc';
+import { CustomerNotification } from 'src/app/model/CustomerNotification';
+import { CommonService } from 'src/app/services/common/common.service';
+import { CustomerService } from 'src/app/services/customer/customer.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  listNoti: Array<CustomerNotification> = [];
+
+  constructor(public common: CommonService, private route: Router, private customerSer: CustomerService) { }
+
+  ngOnInit(): void {
+    // this.customerSer.getAllNotificationByIdCustomer(14).subscribe((data => {
+    //   this.listNoti = data;
+    // }))
+
+    this.customerSer.getAllNotificationByIdCustomer(jwt_decode(this.common.getCookie('token_customer'))['sub']).subscribe((data => {
+      this.listNoti = data;
+    }))
+  }
+
+  exit() {
+    this.common.deleteCookie('token_customer');
+    this.route.navigate(['login-customerweb']);
+  }
+
+}
---
+++ src/app/customerwebsite/home-customer/home-customer.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/services/common/common.service';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
+import jwt_decode from 'jwt-decode';
+import { EmployeeInfoDTO } from 'src/app/model/EmployeeInfoDTO';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatDialog } from '@angular/material/dialog';
+import { CustomerService } from 'src/app/services/customer/customer.service';
+import { Province } from 'src/app/model/Province';
+import { GetContactInfoComponent } from '../get-contact-info/get-contact-info.component';
+
+@Component({
+  selector: 'app-home-customer',
+  templateUrl: './home-customer.component.html',
+  styleUrls: ['./home-customer.component.css']
+})
+export class HomeCustomerComponent implements OnInit {
+
+  listProvince: Array<Province>;
+
+  constructor(private dialog: MatDialog, public common: CommonService, private router: Router, private route: Router,
+    private employeeSer: EmployeeService, private spinner: NgxSpinnerService, private customerService: CustomerService) { }
+  id_role = "";
+  empolyeeInfo: EmployeeInfoDTO;
+  ngOnInit(): void {
+    var code_em = this.common.getCookie('token_key');
+    if (code_em) {
+      this.spinner.show();
+      this.employeeSer.getDetailEmployebyCode(jwt_decode(code_em)['sub']).subscribe((data => {
+        this.empolyeeInfo = data;
+        this.employeeSer.getAccByCode(this.common.getCookie('token_key')).subscribe((data => {
+          this.id_role = data['id_role'];
+          if (this.id_role == '2') {
+            this.spinner.hide();
+            this.router.navigate(['dashboard']);
+          } else if (this.id_role == '1') {
+            this.spinner.hide();
+            this.router.navigate(['employee-manage']);
+          } else if (this.id_role == '3') {
+            this.spinner.hide();
+            this.router.navigate(['appraiser-request-manage']);
+          }
+          this.spinner.hide();
+        }));
+      }))
+    }
+    if (this.common.getSession('contact') == null) {
+      let dialogRef = this.dialog.open(GetContactInfoComponent, {});
+      this.common.setSession('contact', 'ok');
+    }
+    
+  }
+
+  exit() {
+    this.common.deleteCookie('token_customer');
+    this.route.navigate(['login-customer']);
+  }
+
+}
